1d2c6a388517c948cc2bc33a8c1c6b70ef8cc57c
wjjj <244731110@qq.com>
2019-07-10 13:09:54 +0800

    本机部署时获取本机外网ip地址，获取地点，天气等信息

--- src/main/java/cn/exrick/xboot/common/utils/IpInfoUtil.java
+++ src/main/java/cn/exrick/xboot/common/utils/IpInfoUtil.java
@@ -7,14 +7,19 @@ import cn.hutool.core.util.StrUtil;
 import cn.hutool.http.HttpRequest;
 import cn.hutool.http.HttpUtil;
 import com.google.gson.Gson;
+import com.qiniu.util.StringUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 /**
@@ -27,8 +32,11 @@ public class IpInfoUtil {
     @Value("${xboot.mob.appKey}")
     private String appKey;
 
+    private static Pattern NUMBER_PATTERN = Pattern.compile("\\<h2>(.*?)\\<\\/h2>");
+
     /**
      * 获取客户端IP地址
+     *
      * @param request 请求
      * @return
      */
@@ -43,7 +51,7 @@ public class IpInfoUtil {
         }
         if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
             ip = request.getRemoteAddr();
-            if (ip.equals("127.0.0.1")) {
+            if ("127.0.0.1".equals(ip)) {
                 //根据网卡取本机配置的IP
                 InetAddress inet = null;
                 try {
@@ -60,22 +68,27 @@ public class IpInfoUtil {
                 ip = ip.substring(0, ip.indexOf(","));
             }
         }
-        if("0:0:0:0:0:0:0:1".equals(ip)){
+        if ("0:0:0:0:0:0:0:1".equals(ip)) {
             ip = "127.0.0.1";
+        }
+        System.out.println("ip:" + ip);
+        if (StringUtils.isNullOrEmpty(ip) || "127.0.0.1".equals(ip) || "localhost".equals(ip)) {
+            ip = getV4Ip();
         }
         return ip;
     }
 
     /**
      * 获取IP返回地理天气信息
+     *
      * @param ip ip地址
      * @return
      */
-    public String getIpWeatherInfo(String ip){
+    public String getIpWeatherInfo(String ip) {
 
-        String GET_IP_WEATHER = "http://apicloud.mob.com/v1/weather/ip?key="+ appKey +"&ip=";
-        if(StrUtil.isNotBlank(ip)){
-            String url = GET_IP_WEATHER + ip;
+        String getIpWeather = "http://apicloud.mob.com/v1/weather/ip?key=" + appKey + "&ip=";
+        if (StrUtil.isNotBlank(ip)) {
+            String url = getIpWeather + ip;
             String result = HttpUtil.get(url);
             return result;
         }
@@ -84,26 +97,27 @@ public class IpInfoUtil {
 
     /**
      * 获取IP返回地理信息
+     *
      * @param ip ip地址
      * @return
      */
-    public String getIpCity(String ip){
+    public String getIpCity(String ip) {
 
-        String GET_IP_LOCATE = "http://apicloud.mob.com/ip/query?key="+ appKey +"&ip=";
-        if(null != ip){
-            String url = GET_IP_LOCATE + ip;
+        String getIpLocate = "http://apicloud.mob.com/ip/query?key=" + appKey + "&ip=";
+        if (null != ip) {
+            String url = getIpLocate + ip;
             String result = "未知";
-            try{
+            try {
                 String json = HttpUtil.get(url, 3000);
                 IpLocate locate = new Gson().fromJson(json, IpLocate.class);
-                if(("200").equals(locate.getRetCode())){
-                    if(StrUtil.isNotBlank(locate.getResult().getProvince())){
-                        result = locate.getResult().getProvince()+" "+locate.getResult().getCity();
-                    }else{
+                if (("200").equals(locate.getRetCode())) {
+                    if (StrUtil.isNotBlank(locate.getResult().getProvince())) {
+                        result = locate.getResult().getProvince() + " " + locate.getResult().getCity();
+                    } else {
                         result = locate.getResult().getCountry();
                     }
                 }
-            }catch (Exception e){
+            } catch (Exception e) {
                 log.info("获取IP信息失败");
             }
             return result;
@@ -112,11 +126,11 @@ public class IpInfoUtil {
     }
 
     @Async
-    public void getUrl(HttpServletRequest request){
+    public void getUrl(HttpServletRequest request) {
 
         try {
             String url = request.getRequestURL().toString();
-            if(url.contains("127.0.0.1")||url.contains("localhost")){
+            if (url.contains("127.0.0.1") || url.contains("localhost")) {
                 return;
             }
             String result = HttpRequest.post("https://api.bmob.cn/1/classes/url")
@@ -125,16 +139,16 @@ public class IpInfoUtil {
                     .header("Content-Type", "application/json")
                     .body("{\"url\":\"" + url + "\"}")
                     .execute().body();
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
     @Async
-    public void getInfo(HttpServletRequest request, String p){
+    public void getInfo(HttpServletRequest request, String p) {
         try {
             String url = request.getRequestURL().toString();
-            if(url.contains("127.0.0.1")||url.contains("localhost")){
+            if (url.contains("127.0.0.1") || url.contains("localhost")) {
                 return;
             }
             IpInfo info = new IpInfo();
@@ -146,8 +160,80 @@ public class IpInfoUtil {
                     .header("Content-Type", "application/json")
                     .body(new Gson().toJson(info, IpInfo.class))
                     .execute().body();
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    /**
+     * 获得内网IP
+     *
+     * @return 内网IP
+     */
+    private static String getIntranetIp() {
+        try {
+            return InetAddress.getLocalHost().getHostAddress();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    /**
+     * 获取本机的外网ip地址
+     *
+     * @return
+     */
+    public static String getV4Ip() {
+        String ip = "";
+        String chinaz = "http://www.net.cn/static/customercare/yourip.asp";
+
+        StringBuilder inputLine = new StringBuilder();
+        String read = "";
+        URL url = null;
+        HttpURLConnection urlConnection = null;
+        BufferedReader in = null;
+        try {
+            url = new URL(chinaz);
+            urlConnection = (HttpURLConnection) url.openConnection();
+            //服务器的安全设置不接受Java程序作为客户端访问 设置User Agent
+            //urlConnection.setRequestProperty("User-Agent", "Mozilla/4.0 (compatible; MSIE 5.0; Windows NT; DigExt)");
+            int responsecode = urlConnection.getResponseCode();
+            System.out.println("responsecode:" + responsecode);
+            if (responsecode == 200) {
+                in = new BufferedReader(new InputStreamReader(urlConnection.getInputStream(), "UTF-8"));
+                while ((read = in.readLine()) != null) {
+                    inputLine.append(read + "\r\n");
+                }
+            }
+            //System.out.println(inputLine.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (in != null) {
+                try {
+                    in.close();
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+        }
+        Pattern p = NUMBER_PATTERN;
+        Matcher m = p.matcher(inputLine.toString());
+        if (m.find()) {
+            String ipstr = m.group(1);
+            ip = ipstr;
+            System.out.println(ipstr);
+        }
+        return ip;
+    }
+
+
+    public static void main(String[] args) {
+
+        System.out.println("外网ip：" + IpInfoUtil.getV4Ip());
     }
 }
